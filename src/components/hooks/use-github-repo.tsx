fa575feeefc4bf09da4873122d444e6d4f4c1b91
Chris Ackerman <chris@topher.land>
2023-01-06 08:17:20 -0800

    Disabling git repo refetch on reconnect/focus

--- src/components/hooks/use-github-repo.tsx
+++ src/components/hooks/use-github-repo.tsx
@@ -12,24 +12,31 @@ const useGitHubRepo = (owner: string, repo: string): QueryResult<Repo> => {
   const url = `https://api.github.com/repos/${owner}/${repo}`;
   const key = [url];
   const repoCache = useCache<Repo>(key, { lifetime: 21_600_000 /* 6 hours */ });
-  const repoResult = useQuery(key, async ({ signal }) =>
-    repoCache.resolve(async (cached, isFresh) => {
-      if (isFresh) {
-        return cached;
-      }
-
-      const res = await fetch(url, { signal });
-
-      if (!res.ok) {
-        if (!cached) {
-          throw new Error(`Failed getting repo (status: ${res.status}, owner: ${owner}, repo: ${repo})`);
+  const repoResult = useQuery(
+    key,
+    async ({ signal }) => {
+      return repoCache.resolve(async (cached, isFresh) => {
+        if (isFresh) {
+          return cached;
         }
 
-        return cached;
-      }
+        const res = await fetch(url, { signal });
 
-      return await res.json();
-    }),
+        if (!res.ok) {
+          if (!cached) {
+            throw new Error(`Failed getting repo (status: ${res.status}, owner: ${owner}, repo: ${repo})`);
+          }
+
+          return cached;
+        }
+
+        return await res.json();
+      });
+    },
+    {
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    },
   );
 
   return repoResult;
